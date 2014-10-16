c71d6ca07dc58ecdb844ba04515e8003662c1740
Sergey Haritonchik <skharitonchik@exadel.com>
2014-10-16 14:53:03 +0300

    add "/phone" and "/getProducts" methods

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ function fileSend(req, resp) {
     resp.sendfile(__dirname + req.path);
 }
 
+function getPageRelatedPath(req, resp) {
+    console.log(req.method + ': ' + req.path);
+    resp.sendfile(__dirname + '/pages' + req.path + '.html');
+}
+
+function getProductsDb(req, resp){
+    var productsPath = '/db/products.json';
+    console.log(req.method + ': ' + productsPath);
+    resp.sendfile(__dirname + productsPath);
+}
+
 app.get('/', function (req, resp) {
     console.log(req.method + ': ' + req.path);
     resp.sendfile(__dirname + "/index.html");
@@ -23,6 +34,9 @@ app.get('/scripts/*', fileSend);
 app.get('/pages/*', fileSend);
 app.get('/images/*', fileSend);
 app.get('/styles/*', fileSend);
+
+app.get('/getProducts', getProductsDb);
+app.get('/phone', getPageRelatedPath);
 
 
 http.createServer(app).listen(8081, function () {
